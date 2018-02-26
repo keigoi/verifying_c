7ac363120b5fa9a7cfdeed414d1fb14ca195d931
Keigo Imai <keigo.imai@gmail.com>
2018-02-26 12:32:41 +0900

    remove irrelevant parts

--- pqueue_mariadb/queues.h
+++ pqueue_mariadb/queues.h
@@ -40,8 +40,6 @@ extern "C" {
 
 typedef unsigned int uint;
 typedef unsigned char uchar;
-typedef char	pchar;
-typedef char	pbool;
 typedef int compare_fun(void * env, uchar * a,uchar * b);
 
 typedef struct st_queue {
@@ -56,39 +54,7 @@ typedef struct st_queue {
   compare_fun *compare;
 } QUEUE;
 
-#define queue_first_element(queue) 1
-#define queue_last_element(queue) (queue)->elements
-#define queue_empty(queue) ((queue)->elements == 0)
-#define queue_top(queue) ((queue)->root[1])
-#define queue_element(queue,index) ((queue)->root[index])
-#define queue_end(queue) ((queue)->root[(queue)->elements])
-#define queue_replace_top(queue) _downheap(queue, 1, (queue)->root[1])
-#define queue_set_cmp_arg(queue, set_arg) (queue)->first_cmp_arg= set_arg
-#define queue_set_max_at_top(queue, set_arg) \
-  (queue)->max_at_top= set_arg ? -1 : 1
-#define queue_remove_top(queue_arg) queue_remove((queue_arg), queue_first_element(queue_arg))
-typedef compare_fun *queue_compare;
-
-int init_queue(QUEUE *queue,uint max_elements,uint offset_to_key,
-	       pbool max_at_top, queue_compare compare,
-	       void *first_cmp_arg, uint offset_to_queue_pos,
-               uint auto_extent);
-int reinit_queue(QUEUE *queue,uint max_elements,uint offset_to_key,
-                 pbool max_at_top, queue_compare compare,
-                 void *first_cmp_arg, uint offset_to_queue_pos,
-                 uint auto_extent);
-int resize_queue(QUEUE *queue, uint max_elements);
-void delete_queue(QUEUE *queue);
-void queue_insert(QUEUE *queue,uchar *element);
-int queue_insert_safe(QUEUE *queue, uchar *element);
-uchar *queue_remove(QUEUE *queue,uint idx);
-void queue_replace(QUEUE *queue,uint idx);
-
-#define queue_remove_all(queue) { (queue)->elements= 0; }
-#define queue_is_full(queue) (queue->elements == queue->max_elements)
 void _downheap(QUEUE *queue, uint idx, uchar *element);
-void queue_fix(QUEUE *queue);
-#define is_queue_inited(queue) ((queue)->root != 0)
 
 #ifdef	__cplusplus
 }
